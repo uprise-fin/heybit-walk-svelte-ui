868dc223c45106380f6ba8857b0f45a2e763ea09
hckang80 <hckang80@gmail.com>
2024-06-21 18:04:56 +0900

    fix: type error

--- src/stories/Button.svelte
+++ src/stories/Button.svelte
@@ -1,36 +1,36 @@
 <script>
-  import './button.css';
+	import './button.css';
 
-  /**
-   * Is this the principal call to action on the page?
-   */
-  export let primary = false;
+	/**
+	 * Is this the principal call to action on the page?
+	 */
+	export let primary = false;
 
-  /**
-   * @type {string} What background color to use
-   */
-  export let backgroundColor = undefined;
+	/**
+	 * @type {string} What background color to use
+	 */
+	export let backgroundColor = '#fff';
 
-  /**
-   * @type {'small' | 'medium' | 'large'} How large should the button be?
-   */
-  export let size = 'medium';
+	/**
+	 * @type {'small' | 'medium' | 'large'} How large should the button be?
+	 */
+	export let size = 'medium';
 
-  /**
-   * @type {string} Button contents
-   */
-  export let label;
+	/**
+	 * @type {string} Button contents
+	 */
+	export let label;
 
-  $: mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+	$: mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
 
-  $: style = backgroundColor ? `background-color: ${backgroundColor}` : '';
+	$: style = backgroundColor ? `background-color: ${backgroundColor}` : '';
 </script>
 
 <button
-  type="button"
-  class={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-  {style}
-  on:click
+	type="button"
+	class={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+	{style}
+	on:click
 >
-  {label}
+	{label}
 </button>
