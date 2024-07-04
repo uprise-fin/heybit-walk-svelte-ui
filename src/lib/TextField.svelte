87d9164ec2d5364c13ec5d7ee54dc54634f7ab5c
hckang80 <hckang80@gmail.com>
2024-07-04 10:46:32 +0900

    feat: textField 추가

---
+++ src/lib/TextField.svelte
@@ -0,0 +1,220 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+
+  export let id = '';
+  export let disabled = false;
+  export let readonly = false;
+  export let value = '';
+  export let errorMessages: string[] = [];
+  export let label = '';
+  export let clearable = true;
+  export let descriptions: string[] = [];
+
+  const dispatcher = createEventDispatcher<{
+    input: Event & {
+      currentTarget: EventTarget & HTMLInputElement;
+    };
+  }>();
+
+  $: hasError = errorMessages?.length;
+
+  let inputEl: HTMLInputElement;
+
+  const randomId = Math.random().toString(36).slice(2);
+  const _id = id || randomId;
+
+  const clear = () => {
+    value = '';
+    inputEl.focus();
+  };
+
+  $: messages = errorMessages?.length ? errorMessages : descriptions;
+</script>
+
+<div class="input-wrap">
+  {#if label}
+    <label class="input-wrap__label" class:input-wrap__label--error={hasError} for={_id}>
+      {label}
+    </label>
+  {/if}
+  <div class="input" class:input--error={hasError} class:input--disabled={disabled || readonly}>
+    <span class="input__prefix">
+      <input
+        bind:this={inputEl}
+        bind:value
+        {...$$restProps}
+        class="input__el"
+        id={_id}
+        {disabled}
+        {readonly}
+        on:input={(event) => dispatcher('input', event)}
+        on:focus
+        on:blur
+      />
+      {#if clearable && value && !disabled && !readonly}
+        <button type="button" class="input__clear" on:click={clear}>
+          <span class="input__clear-inner">clear</span>
+        </button>
+      {/if}
+    </span>
+    <span class="input__suffix"><slot /></span>
+  </div>
+
+  {#if !!messages?.length}
+    <div class="messages" class:messages--error={hasError}>
+      {#each messages as message}
+        <p class="messages__item">{message}</p>
+      {/each}
+    </div>
+  {/if}
+</div>
+
+<style lang="scss">
+  .input-wrap {
+    --color: var(--walk__black);
+    color: var(--color);
+
+    &__label {
+      display: block;
+      margin: 0 0 4px;
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 1.5;
+    }
+  }
+
+  .input {
+    --padding: 16px;
+    display: flex;
+    align-items: center;
+    padding-inline: var(--padding);
+    outline: 1px solid var(--walk__black--300);
+    border-radius: 12px;
+    background: #fff;
+    transition: outline 0.1s;
+
+    &:focus-within:not(.input--disabled) {
+      outline-color: var(--walk__black);
+    }
+
+    &--disabled {
+      background: var(--walk__black--100);
+    }
+
+    &--error {
+      outline: 1px solid var(--walk__warning--500) !important;
+    }
+
+    &__prefix {
+      flex: 1 1 auto;
+      display: flex;
+      align-items: center;
+    }
+
+    &__el {
+      box-sizing: border-box;
+      width: 100%;
+      height: 48px;
+      padding-block: var(--padding);
+      background: none;
+      border: 0;
+      outline: 0;
+      font-size: 15px;
+      line-height: 1.2;
+      color: inherit;
+      caret-color: var(--walk__black);
+      &:read-only {
+        -webkit-text-fill-color: var(--walk__black--400);
+      }
+      &::placeholder,
+      &:disabled {
+        -webkit-text-fill-color: var(--walk__black--400);
+      }
+    }
+
+    &__clear {
+      --size: 16px;
+      all: unset;
+      opacity: 0;
+      flex: 0 0 auto;
+      margin: 0 0 0 10px;
+      padding: 4px;
+      font-size: 0.1em;
+      color: #fff;
+      cursor: pointer;
+
+      .input:focus-within & {
+        opacity: 1;
+      }
+    }
+
+    &__clear-inner {
+      display: inline-grid;
+      place-items: center;
+      position: relative;
+      width: var(--size);
+      height: var(--size);
+      border-radius: 100%;
+      background: var(--walk__black--300);
+      text-indent: -9999px;
+      font-size: 0.1em;
+
+      &::before,
+      &::after {
+        content: '';
+        position: absolute;
+        width: 2px;
+        height: 8px;
+        border-radius: 4px;
+        background: #fff;
+      }
+
+      &::before {
+        transform: rotate(45deg);
+      }
+      &::after {
+        transform: rotate(-45deg);
+      }
+    }
+
+    &__suffix {
+      display: grid;
+      place-items: center;
+      color: var(--on-neutral-caption);
+
+      &:not(:empty) {
+        margin: 0 0 0 8px;
+      }
+    }
+  }
+
+  .messages {
+    --color: var(--walk__black);
+    margin: 4px 0 0;
+    color: var(--color);
+    font-size: 12px;
+    line-height: 1.5;
+
+    &--error {
+      --color: var(--walk__warning--500);
+    }
+
+    &__item {
+      position: relative;
+      margin: 0;
+      padding: 0 0 0 14px;
+
+      &::before {
+        --size: 3px;
+        content: '';
+        position: absolute;
+        left: 2.5px;
+        top: 8.5px;
+        width: var(--size);
+        height: var(--size);
+        border-radius: 100%;
+        background: var(--color);
+      }
+    }
+  }
+</style>
---
+++ src/stories/TextField.stories.ts
@@ -0,0 +1,55 @@
+import { TextField } from '$lib';
+import type { Meta, StoryObj } from '@storybook/svelte';
+
+const meta = {
+  title: 'Component/TextField',
+  component: TextField,
+  tags: ['autodocs'],
+  argTypes: {
+    value: {
+      description: 'bind로 전달하면 양방향 바인딩됩니다.'
+    },
+    id: {
+      table: {
+        disable: true
+      }
+    },
+    clearable: {
+      description: '요소 안 우측의 X버튼 노출 여부를 결정합니다.'
+    }
+  }
+} satisfies Meta<TextField>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Basic: Story = {
+  args: {
+    label: '라벨',
+    disabled: false,
+    placeholder: '플레이스 홀더',
+    value: '입력한 정보',
+    descriptions: ['설명글']
+  }
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+    value: '입력한 정보'
+  }
+};
+
+export const Readonly: Story = {
+  args: {
+    readonly: true,
+    value: '입력한 정보'
+  }
+};
+
+export const Error: Story = {
+  args: {
+    label: '라벨',
+    errorMessages: ['에러 힌트']
+  }
+};
