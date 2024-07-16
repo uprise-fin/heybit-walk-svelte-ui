59ccd5d0d5a5f03c6dad1d222cefa5292b117ebf
hckang80 <hckang80@gmail.com>
2024-07-16 17:10:38 +0900

    feat(groupButton): 슬롯 영역 디자인 고도화

--- src/lib/GroupButton.svelte
+++ src/lib/GroupButton.svelte
@@ -5,35 +5,40 @@
   export let footers: Partial<ButtonOption>[] | undefined = undefined;
 </script>
 
-<div class="group-button" class:is-vertical={isVerticalLayout}>
+<div class="group-button">
   <slot />
-  {#if footers}
-    {#each footers as footer}
-      <Button {...footer} on:click={footer.handler} />
-    {/each}
-  {/if}
+  <div class="group-button__inner" class:is-vertical={isVerticalLayout}>
+    {#if footers}
+      {#each footers as footer}
+        <Button {...footer} on:click={footer.handler} />
+      {/each}
+    {/if}
+  </div>
 </div>
 
 <style lang="scss">
   .group-button {
     --gap: 8px;
-    display: flex;
-    flex-flow: row wrap;
-    justify-content: center;
-    gap: var(--gap);
     padding-block: 20px;
     background: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, #ffffff 100%);
 
+    &__inner {
+      display: flex;
+      flex-flow: row wrap;
+      justify-content: center;
+      gap: var(--gap);
+
+      &.is-vertical {
+        flex-direction: column;
+      }
+
+      &:not(.is-vertical) :global(.button) {
+        flex: 1 1 0;
+      }
+    }
+
     :global(.button--text:not(:first-child)) {
       margin-top: calc(-1 * var(--gap));
-    }
-
-    &:not(.is-vertical) :global(.button) {
-      flex: 1 1 0;
-    }
-
-    &.is-vertical {
-      flex-direction: column;
     }
   }
 </style>
